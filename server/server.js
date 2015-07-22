28698e2519d63855c4c6108cd4ede9396c5dbd6e
Hendrixer <willscottmoss@gmail.com>
2015-07-22 04:52:00 -0500

    chore

--- server/server.js
+++ server/server.js
@@ -2,27 +2,52 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var _ = require('lodash');
+var morgan = require('morgan');
 
+var lions = [];
+var id = 0;
+
+var updateId = function(req, res, next) {
+  if (!req.body.id) {
+    id++;
+    req.body.id = id + '';
+  }
+  next();
+};
+
+app.use(morgan('dev'))
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var lions = [];
-var id = 0;
+app.use(function(err, req, res, next) {
+  if (err) {
+    res.status(500).send(error);
+  }
+});
+
+app.param('id', function(req, res, next, id) {
+  var todo = _.find(todos, {id: id});
+
+  if (todo) {
+    req.todo = todo;
+    next();
+  } else {
+    res.send();
+  }
+});
 
 app.get('/lions', function(req, res){
   res.json(lions);
 });
 
 app.get('/lions/:id', function(req, res){
-  var lion = _.find(lions, {id: req.params.id});
+  var lion = req.todo;
   res.json(lion || {});
 });
 
-app.post('/lions', function(req, res) {
+app.post('/lions', updateId, function(req, res) {
   var lion = req.body;
-  id++;
-  lion.id = id + '';
 
   lions.push(lion);
 
