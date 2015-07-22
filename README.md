1422215da2675bb412f0e1a4e234a48d0dae4acf
Hendrixer <willscottmoss@gmail.com>
2015-07-22 05:50:46 -0500

    chore

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-##Solution to step-1
+##Testing our app
+
+We're going to use mocha and chai along with supertest to runt inegration test against our api. **Be sure to gloably install mocha**. `npm i -g mocha`. 
+
+To run the test, run `mocha server/spec.js` or `npm test`;
--- server/spec.js
+++ server/spec.js
@@ -2,6 +2,10 @@ var app = require('./server');
 var request = require('supertest');
 var expect = require('chai').expect;
 
+// TODO: make tests for the other CRUD routes
+// DELETE, UPDATE, PUT, GET ONE
+// to run the test type mocha server/specs.js
+
 describe('[LIONS]', function(){
 
   it('should get all lions', function(done) {
@@ -13,69 +17,6 @@ describe('[LIONS]', function(){
       .end(function(err, resp) {
         expect(resp.body).to.be.an('array');
         done();
-      })
-  });
-
-  it('should create a lion', function(done) {
-    request(app)
-      .post('/lions')
-      .send({
-        name: 'Mufasa',
-        age: 100,
-        pride: 'Evil lions',
-        gender:'male'
-      })
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(201)
-      .end(function(err, resp) {
-        expect(resp.body).to.be.an('object');
-        done();
-      })
-  });
-
-  it('should delete a lion', function(done) {
-    request(app)
-      .post('/lions')
-      .send({
-        name: 'test lion',
-        age: 100,
-        pride: 'test lion',
-        gender:'female'
-      })
-      .set('Accept', 'application/json')
-      .end(function(err, resp) {
-        var lion = resp.body;
-        request(app)
-          .delete('/lions/' + lion.id)
-          .end(function(err, resp) {
-            expect(resp.body).to.eql(lion);
-            done();
-          });
-      })
-  });
-
-  it('should update a lion', function(done) {
-    request(app)
-      .post('/lions')
-      .send({
-        name: 'test lion',
-        age: 100,
-        pride: 'test lion',
-        gender:'female'
-      })
-      .set('Accept', 'application/json')
-      .end(function(err, resp) {
-        var lion = resp.body;
-        request(app)
-          .put('/lions/' + lion.id)
-          .send({
-            name: 'new name'
-          })
-          .end(function(err, resp) {
-            expect(resp.body.name).to.equal('new name');
-            done();
-          });
       })
   });
 });
