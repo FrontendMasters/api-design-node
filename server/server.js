fb9a2a4ff84fededf661d5c82eb99339bd171d46
Hendrixer <willscottmoss@gmail.com>
2015-07-22 05:09:32 -0500

    chore

--- server/server.js
+++ server/server.js
@@ -4,21 +4,17 @@ var app = express();
 var _ = require('lodash');
 var morgan = require('morgan');
 
-var lions = [];
-var id = 0;
-
-var updateId = function(req, res, next) {
-  if (!req.body.id) {
-    id++;
-    req.body.id = id + '';
-  }
-  next();
-};
+var lionRouter = require('./lions');
+var tigerRouter = require('./tigers');
 
 app.use(morgan('dev'))
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+// this is called mounting. when ever a req comes in for
+// '/lion' we want to use this router
+app.use('/lions', lionRouter);
+app.use('/tigers', tigerRouter);
 
 app.use(function(err, req, res, next) {
   if (err) {
@@ -26,49 +22,7 @@ app.use(function(err, req, res, next) {
   }
 });
 
-app.param('id', function(req, res, next, id) {
-  var todo = _.find(todos, {id: id});
 
-  if (todo) {
-    req.todo = todo;
-    next();
-  } else {
-    res.send();
-  }
-});
-
-app.get('/lions', function(req, res){
-  res.json(lions);
-});
-
-app.get('/lions/:id', function(req, res){
-  var lion = req.todo;
-  res.json(lion || {});
-});
-
-app.post('/lions', updateId, function(req, res) {
-  var lion = req.body;
-
-  lions.push(lion);
-
-  res.json(lion);
-});
-
-
-app.put('/lions/:id', function(req, res) {
-  var update = req.body;
-  if (update.id) {
-    delete update.id
-  }
-
-  var lion = _.findIndex(lions, {id: req.params.id});
-  if (!lions[lion]) {
-    res.send();
-  } else {
-    var updatedLion = _.assign(lions[lion], update);
-    res.json(updatedLion);
-  }
-});
 
 app.listen(3000);
 console.log('on port 3000');
