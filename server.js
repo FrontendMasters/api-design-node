af8e189cca3a92d13019b2897142283fa8e7ec4d
Hendrixer <willscottmoss@gmail.com>
2015-07-22 02:58:25 -0500

    chore

---
+++ server.js
@@ -0,0 +1,4 @@
+// TODO: create a basic server with express
+// that can handle a GET request for what ever resource you'd like
+// whatever url you want, and then navigate to that url in the browser and
+// see if yuor resource is there.
