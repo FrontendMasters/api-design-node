da11b5d0015cf1403f8df538a7df96cb3eb6b916
Hendrixer <willscottmoss@gmail.com>
2015-07-23 07:15:07 -0500

    chore

--- server/api/user/userRoutes.js
+++ server/api/user/userRoutes.js
@@ -1,16 +1,12 @@
 var router = require('express').Router();
 var logger = require('../../util/logger');
-var controller = require('./userController');
+
 // setup boilerplate route jsut to satisfy a request
 // for building
-router.param('id', controller.params);
+router.param('id', function(){});
 
 router.route('/')
-  .get(controller.get)
-  .post(controller.post)
 
 router.route('/:id')
-  .get(controller.getOne)
-  .put(controller.put)
-  .delete(controller.delete)
+
 module.exports = router;
