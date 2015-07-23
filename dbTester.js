a728c1157d42473e089ceae20d75f01178868bc0
Hendrixer <willscottmoss@gmail.com>
2015-07-22 22:08:11 -0500

    chore

--- dbTester.js
+++ dbTester.js
@@ -1,21 +1,24 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/sip');
+mongoose.connect('mongodb://localhost/todos');
 // connect to a database so the below will work.
-// what's happening is that we're querying yor mongo db
-// on the todos collection and
-// logging the results. The todos collection
-// will be created for you so no need to make that.
+// what's happening is that we're createing a new schema
+// and making a todos collection and a Todo model.
+// We then create new Todo.
 // So in the mongo shell in the terminal,
-// or in here if you're feeling ambitions,
-// you need to add some documents
-// to the todos collection.
+// connect to your database and uery it there
+// and see if you see the todo below log in the terminal
 // run node server/db.Tester to execute this file
 // MAKE SURE MONGOD is RUNNING
 
-var TodoSchema = new mongoose.Schema({});
+var TodoSchema = new mongoose.Schema({
+  name: String,
+  completed: Boolean
+});
 
 var Todo = mongoose.model('todo', TodoSchema);
-
-Todo.find({}, function(err, todos) {
-  console.log(err, todos);
+Todo.create({
+  name: 'clean up your room!!!',
+  completed: false
+}).then(function(err, todo) {
+  console.log(err, todo);
 });
