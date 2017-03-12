a6bd27fa0ef98a94fd6d67e06a1b23a445e5bfb4
Mike Sausa <mikejsausa@gmail.com>
2017-03-11 18:37:57 -0700

    exercise 3

--- server/server.js
+++ server/server.js
@@ -1,23 +1,8 @@
-// TODO: make this work.
-// if yuo go to localhost:3000 the app
-// there is expected crud to be working here
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var _ = require('lodash');
-
-// express.static will serve everything
-// with in client as a static resource
-// also, it will server the index.html on the
-// root of that directory on a GET to '/'
-app.use(express.static('client'));
-
-// body parser makes it possible to post JSON to the server
-// we can accss data we post on as req.body
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
+var morgan = require('morgan');
 
 var lions = [];
 var id = 0;
@@ -28,39 +13,60 @@ let findItem = (dataSet, property) => {
   }
 }
 
-app.get('/lions', (req, res) => {
-  res.send(lions);
+var updateId = function(req, res, next) {
+  id++;
+  req.body.id = id + '';
+  next();
+};
+
+app.use(morgan('dev'))
+app.use(express.static('client'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+app.param('id', function(req, res, next, id) {
+  let lion = findItem(lions, id);
+  req.lion = lion;
+  next();
 });
 
-app.get('/lions/:id', (req, res) => {
-  let lion = findItem(lions, 'id');
-  res.send(lion || undefined);
-})
+app.get('/lions', function(req, res){
+  res.json(lions);
+});
 
-app.post('/lions', (req, res) => {
-  let lion = req.body;
-  lion.id = Date.now();
+app.get('/lions/:id', function(req, res){
+  lion = req.lion;
+  res.json(lion || {});
+});
+
+app.post('/lions', updateId, function(req, res) {
+  var lion = req.body;
   lions.push(lion);
-  res.send(lion);
+  res.json(lion);
 });
 
-// check to see if the below method works - it might not be updating in the array
-app.put('/lions/:id', (req, res) => {
-  let update = req.body;
-  let lion = findItem(lions, 'id');
-  let updatedLion = Object.assign(lion, update);
-  res.send(updatedLion)
+
+app.put('/lions/:id', function(req, res) {
+  var update = req.body;
+  if (update.id) {
+    delete update.id
+  }
+
+  var lion = _.findIndex(lions, {id: req.params.id});
+  if (!lions[lion]) {
+    res.send();
+  } else {
+    var updatedLion = _.assign(lions[lion], update);
+    res.json(updatedLion);
+  }
 });
 
-app.delete('/lions/:id', (req, res) => {
-  for(let i=0; i<lions.length; i++){
-    if (lions[i].id === req.params.id){
-      let lion = lions[i];
-      lions.splice(i, 1);
-      res.send(lion);
-    };
-  };
-});
+app.use((err, req, res, next) => {
+  if(err){
+    res.status(500).send(err);
+  }
+})
 
 app.listen(3000);
 console.log('on port 3000');
