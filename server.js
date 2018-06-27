70493651aec2cc7333b0b6517215180ef06e021a
Dontavious Sanders <sanddc178@gmail.com>
2018-06-27 18:12:28 -0500

    Set up basic express server. Completed exercise 1.

--- server.js
+++ server.js
@@ -1,5 +1,30 @@
 // TODO: create a basic server with express
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
+const express = require('express')
+const fs = require('fs')
+const jsonData = { count: 12, message: 'sup' }
+const port = 3000
 
-var jsonData = {count: 12, message: 'hey'};
+const app = express()
+
+app.get('/', (req, res) => {
+  // fs.readFile('index.html', function(err, buffer) {
+  //   const html = buffer.toString()
+
+  //   res.setHeader('Content-Type', 'text/html')
+  //   res.send(html)
+  // })
+
+  res.sendFile(`${__dirname}/index.html`, err => {
+    if (err) res.status(500).send(err)
+  })
+})
+
+app.get('/data', (req, res) => {
+  res.json(jsonData)
+})
+
+app.listen(port, () => {
+  console.log(`Errthang good on ${port}`)
+})
